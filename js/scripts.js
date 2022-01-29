68103ca70eceb03f46532a6c251bd7f018989a49
Katharina Azanza <kat.azanza@gmail.com>
2022-01-29 19:47:45 +0800

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,16 @@
-console.log ("Welcome to my portfolio")
+<script src="https://kit.fontawesome.com/5e64caa599.js" crossorigin="anonymous"></script>
+
+<script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
+
+function menuToggle() {
+  var x = document.getElementById('myNavtoggle');
+  if (x.className === 'navtoggle') {
+    x.className += ' responsive';
+  } else {
+    x.className = 'navtoggle';
+  }
+
+
+}
+
+<script src="js/scripts.js"></script>
