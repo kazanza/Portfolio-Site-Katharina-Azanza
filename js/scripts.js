825fc255873f8259460e52e16f25d16ed46a64eb
Katharina Azanza <kat.azanza@gmail.com>
2022-02-05 22:01:37 +0800

    updated linter errors

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,3 @@
-<script src="https://kit.fontawesome.com/5e64caa599.js" crossorigin="anonymous"></script>
-
-<script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
-
 function menuToggle() {
   var x = document.getElementById('myNavtoggle');
   if (x.className === 'navtoggle') {
@@ -10,7 +6,4 @@ function menuToggle() {
     x.className = 'navtoggle';
   }
 
-
 }
-
-<script src="js/scripts.js"></script>
